31b9b5c06dd833178883d3416056b6cb1023514a
Roque Betiol Jr <roque.betioljr@gmail.com>
2017-10-23 17:40:57 -0200

    Debug print on setting QoS values

--- backports-4.4.2-1/drivers/net/wireless/ath/ath9k/main.c
+++ backports-4.4.2-1/drivers/net/wireless/ath/ath9k/main.c
@@ -1628,12 +1628,14 @@ static int ath9k_conf_tx(struct ieee80211_hw *hw,
 	mutex_lock(&sc->mutex);
 
 	memset(&qi, 0, sizeof(struct ath9k_tx_queue_info));
-	printk("ath9k_conf_tx");
 	qi.tqi_aifs = params->aifs;
 	qi.tqi_cwmin = params->cw_min;
 	qi.tqi_cwmax = params->cw_max;
 	qi.tqi_burstTime = params->txop * 32;
-	printk("ath9k_conf_tx CONFIG: %s", CONFIG);
+
+	printk("ath9k_conf_tx: Configure tx [queue/halq] [%d/%d], aifs: %d, cw_min: %d, cw_max: %d, txop: %d\n",
+		queue, txq->axq_qnum, params->aifs, params->cw_min,
+		params->cw_max, params->txop);
 
 	ath_dbg(common, CONFIG,
 		"Configure tx [queue/halq] [%d/%d], aifs: %d, cw_min: %d, cw_max: %d, txop: %d\n",
